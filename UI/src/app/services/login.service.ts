17590594622683051f64f4e3ab9e41909aef72bd
selvaram-geoad <selvaram.k.ext@geodis.com>
2025-05-05 11:53:59 +0530

    Modified logic of Login service

--- UI/src/app/services/login.service.ts
+++ UI/src/app/services/login.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +10,14 @@ export class LoginService {
   constructor(private http: HttpClient) { }
 
   login(username: any, password: any): Observable<any> {
-    return this.http.post(this.loginUrl, {"username":username,"password": password});
+    return this.http.post(this.loginUrl, {"username":username,"password": password}).pipe(tap((response:any) =>{
+      if(response.message === "success"){
+        // sessionStorage.setItem("username",this.username.toString())
+        sessionStorage.setItem("role",response.role);
+        sessionStorage.setItem("access",response.access);
+      }
+    })
+  );
   }
   getRole(){
     return sessionStorage.getItem('role');
