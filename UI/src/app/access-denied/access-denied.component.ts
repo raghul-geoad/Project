e1fbb565ccb810f031c50c91a92275282b5011da
raghul-geoad <raghul.ulagappan.ext@geodis.com>
2025-05-05 15:40:11 +0530

    added snackbars in the request

--- UI/src/app/access-denied/access-denied.component.ts
+++ UI/src/app/access-denied/access-denied.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
 
 @Component({
@@ -11,7 +12,7 @@ export class AccessDeniedComponent implements OnInit {
 
   public user=sessionStorage.getItem('username')
   public componentName:any;
-  constructor(private http:HttpClient,private router:Router) { 
+  constructor(private http:HttpClient,private router:Router,private snackBar:MatSnackBar) { 
     const nav = this.router.getCurrentNavigation();
     this.componentName = nav?.extras.state?.['componentName'];
   }
@@ -20,7 +21,15 @@ export class AccessDeniedComponent implements OnInit {
   }
   
   requestAccess(){
-    this.http.post("http://127.0.0.1:5000/componentAccessRequest",{username:this.user,component:this.componentName}).subscribe((data)=>console.log(data),(err)=>console.error(err))
+    this.http.post("http://127.0.0.1:5000/componentAccessRequest",{username:this.user,component:this.componentName}).subscribe({
+      next : (data:any)=>{
+        const snackBarRef = this.snackBar.open(data.message  ,'Close',{duration:3000});
+      },
+      error : (err)=>{
+        this.snackBar.open('Request Failed','Close',{duration:3000});
+        console.error(err);
+      }
+  });
   }
 
 }
